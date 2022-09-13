bb65c203986190b9a61eb3bbe04e1f70f27c37e8
Shirali <shiralialihummatov@gmail.com>
2022-09-13 20:35:10 +0400

    in progress..

--- src/main/java/util/IdUtil.java
+++ src/main/java/util/IdUtil.java
@@ -1,11 +1,13 @@
 package util;
 
+import model.IdFIle;
+
 import java.io.*;
 import java.util.Optional;
 
 public class IdUtil {
   private int id;
-  private final String idFilePath = "src/main/java/database/UserIdFile.txt";
+  private static final String dbFolderPath = "src/main/java/database/";
   private static final IdUtil idUtil = new IdUtil();
 
   private IdUtil() {
@@ -15,18 +17,18 @@ public class IdUtil {
     return IdUtil.idUtil;
   }
 
-  public Optional<Integer> getNewId() {
-    try (BufferedReader br = new BufferedReader(new FileReader(idFilePath))) {
+  public static Optional<Integer> getNewId(IdFIle fileName) {
+    try (BufferedReader br = new BufferedReader(new FileReader(dbFolderPath+fileName.getFileName()))) {
       int id = Integer.parseInt(br.readLine());
-      idIncrement(id);
+      getInstance().idIncrement(id, fileName);
       return Optional.of(id);
     } catch (IOException e) {
       return Optional.empty();
     }
   }
 
-  private void idIncrement(int id) {
-    try (BufferedWriter bw = new BufferedWriter(new FileWriter(idFilePath))) {
+  private void idIncrement(int id, IdFIle fileName) {
+    try (BufferedWriter bw = new BufferedWriter(new FileWriter(dbFolderPath+fileName.getFileName()))) {
       bw.write(id + 1);
     } catch (IOException e) {
       throw new RuntimeException("There is problem with id generator.");
