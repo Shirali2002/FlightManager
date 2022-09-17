56ab0e1dde247e50d6b9ac3d141d702c368f9e54
Shirali <shiralialihummatov@gmail.com>
2022-09-17 17:08:11 +0400

    in progress..

---
+++ src/main/java/command/CancelTicket.java
@@ -0,0 +1,38 @@
+package command;
+
+import app.FlightManager;
+import console.Console;
+import console.RealConsole;
+import controller.BookingController;
+import model.Ticket;
+import util.ConsoleUtil;
+
+import java.util.Optional;
+
+public class CancelTicket {
+  public static void cancelTicket() {
+    cancelTicket(new RealConsole());
+  }
+
+  public static void cancelTicket(Console console) {
+    BookingController bc = BookingController.getInstance();
+    bc.displayUserBookings(FlightManager.getLoggedInUserId(), console);
+    int inputId = ConsoleUtil.getInt("Please enter booking id which you want to cancel:");
+    Optional<Ticket> bookingById = bc.getBookingById(inputId);
+    if (bookingById.isPresent()) {
+      Ticket ticket = bookingById.get();
+      if (ticket.getUserIdWhoOrderedTicket() == FlightManager.getLoggedInUserId()) {
+        if (bc.cancelBooking(inputId)) {
+          console.printLine("Canceled successfully.");
+        } else {
+          console.printLine("There is problem. Ticket can not canceled");
+        }
+      } else {
+        console.printLine("This ticket is not ordered by you so you can not cancel this ticket.");
+      }
+    } else {
+      console.printLine("There is no booking with this id.");
+    }
+
+  }
+}
---
+++ src/main/java/command/OnlineBoard.java
@@ -0,0 +1,18 @@
+package command;
+
+import console.Console;
+import console.RealConsole;
+import controller.FlightController;
+import util.ConsoleUtil;
+
+public class OnlineBoard {
+  public static void showOnlineBoard() {
+    showOnlineBoard(new RealConsole());
+  }
+
+  public static void showOnlineBoard(Console console) {
+    int hours = ConsoleUtil.getInt("How many hours you want to see: ");
+    FlightController.getInstance().getAllFlightsNextHours(hours)
+        .forEach(f -> console.printLine(f.prettyFormat()));
+  }
+}
