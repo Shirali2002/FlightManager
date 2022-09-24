5cbd9c24345f1e78c495d32a187be4ce81610546
Shirali2002 <shiralialihummatov@gmail.com>
2022-09-24 20:31:20 +0400

    changes

---
+++ src/test/java/service/FlightServiceTest.java
@@ -0,0 +1,84 @@
+package service;
+
+import dao.FlightRepository;
+import model.Airline;
+import model.Airport;
+import model.Flight;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import util.FlightDate;
+
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.List;
+
+public class FlightServiceTest {
+  FlightService flightService = FlightService.getInstance();
+  FlightRepository flightDao = FlightRepository.getInstance();
+  HashMap<Integer, Flight> realData;
+  Flight testFlight;
+
+
+  @BeforeEach
+  public void setTestEnv() {
+    realData = flightService.getAllFlight();
+    flightDao.save(new HashMap<>());
+    testFlight = new Flight(
+        LocalDateTime.now().plusHours(2),
+        Duration.ofHours(2),
+        Airport.KYIV_INTERNATIONAL_AIRPORT,
+        Airport.ISTANBUL_AIRPORT,
+        Airline.AIR_FRANCE,
+        200);
+
+  }
+
+  @AfterEach
+  public void endTestEnv() {
+    flightDao.save(realData);
+  }
+
+  @Test
+  public void testAddFlight(){
+    Flight resFlight = addTestFlight(testFlight);
+    Assertions.assertEquals(testFlight, resFlight);
+  }
+
+  @Test
+  public void testCancelFlightById(){
+    Flight resFlight = addTestFlight(testFlight);
+    flightService.cancelFlightById(resFlight.getId());
+    Assertions.assertEquals(new HashMap<Integer, Flight>() ,flightService.getAllFlight());
+  }
+
+  @Test
+  public void testGetAllFlightsNextHours(){
+    addTestFlight(testFlight);
+    List<Flight> nextHoursFlights = flightService.getAllFlightsNextHours(24);
+    Assertions.assertEquals(1, nextHoursFlights.size());
+  }
+
+
+  @Test
+  public void testSearchFlightByFields(){
+    addTestFlight(testFlight);
+    LocalDateTime startDate = testFlight.getStartDate();
+    List<Flight> resFlights = flightService.searchFlightByFields(
+        List.of(testFlight.getFromWhere()),
+        List.of(testFlight.getToWhere()),
+        new FlightDate(startDate.getYear(), startDate.getMonthValue(), startDate.getDayOfMonth()),
+        1
+    );
+    Assertions.assertEquals(1, resFlights.size());
+  }
+
+
+  private Flight addTestFlight(Flight testFlight){
+    flightService.addFlight(testFlight);
+    return flightService.getAllFlight()
+        .get(testFlight.getId());
+  }
+}
---
+++ src/test/java/service/UserServiceTest.java
@@ -0,0 +1,60 @@
+package service;
+
+import app.FlightManager;
+import dao.UserRepository;
+import model.User;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+
+import java.util.HashMap;
+import java.util.Optional;
+
+
+public class UserServiceTest {
+  UserService userService = UserService.getInstance();
+  UserRepository userDao = UserRepository.getInstance();
+  HashMap<Integer, User> realData;
+  User testUser;
+
+
+  @BeforeEach
+  public void setTestEnv() {
+    realData = userService.getAllUser();
+    userDao.save(new HashMap<>());
+    testUser = new User("Shirali", "Alihummatov", "shirali02", "SH1234sh");
+  }
+
+  @AfterEach
+  public void endTestEnv() {
+    userDao.save(realData);
+  }
+
+  @Test
+  public void testRegisterUser() {
+    Optional<User> result = registerTestUser(testUser);
+    Assertions.assertEquals(testUser.getUsername(), result.get().getUsername());
+  }
+
+  @Test
+  public void testLoginUser() {
+    Optional<User> result = registerTestUser(testUser);
+    userService.login(testUser.getUsername(), testUser.getPassword());
+    Assertions.assertEquals(result.get().getId(), FlightManager.getLoggedInUserId());
+  }
+
+  private Optional<User> registerTestUser(User testUser){
+    userService.register(testUser.getName(), testUser.getSurname(), testUser.getUsername(), testUser.getPassword());
+     return userService.getAllUser().values()
+        .stream().filter(u ->
+            u.getName().equals(testUser.getName())
+                && u.getSurname().equals(testUser.getSurname())
+                && u.getUsername().equals(testUser.getUsername())
+                && u.getPassword().equals(testUser.getPassword())
+        ).findFirst();
+
+  }
+}
